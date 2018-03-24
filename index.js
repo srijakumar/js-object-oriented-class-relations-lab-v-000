95edbfb54e097f9b7d28c3daba9cf423ff47dba8
Srija S <srijakumar7@gmail.com>
2018-03-24 17:32:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,14 +56,14 @@ class Trip {
 
     driver(){
       return store.drivers.find(driver => {
-        return driver.id == this.driverId;
+        return driver.id === this.driverId;
       })
     }
 
     passenger(){
       return store.passengers.find(passenger => {
-        return passenger.id == this.passengerId;
-      })
+        return passenger.id === this.passengerId;
+      });
     }
 
 }
