7da6cf9830868b60e64e3de4b606e749409f3f34
Srija S <srijakumar7@gmail.com>
2018-03-24 17:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ class Driver{
     this.name = name;
     this.id = driverId++;
     store.drivers.push(this);
-  }
+  };
   trips(){
     return store.trips.filter( trip => {
     return trip.driverId==this.id;
-  })
+  });
   }
 
   passengers(){
     return store.passengers.filter(passenger =>{
       return passenger.driverId == this.id;
-    })
+    });
   }
 
 }
